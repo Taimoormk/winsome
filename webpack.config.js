9444cbcb1516bfb600c5cab088305dde8d6d36e0
Taimoormk <taimoor.m.k@live.com>
2018-01-24 11:19:50 +0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+import { ExtendedAPIPlugin } from '../Library/Caches/typescript/2.6/node_modules/@types/webpack';
+
+var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+const glob = require('glob-all');
+var PurifyCSSPlugin = require('purifycss-webpack');
+
+module.exports = {
+  entry: './src/js/app.js',
+  output: {
+    path: __dirname,
+    filename: 'dist/js/bundle.js'
+  },
+  watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: 'node_modules',
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env'],
+            plugins: ['transform-object-rest-spread']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                url: flase
+              }
+            }
+          ]
+        })
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                url: flase
+              }
+            },
+            'sass-loader'
+          ]
+        })
+      }
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      '$': 'jquery',
+      'jQuery': 'jquery'
+    }),
+    new ExtractTextPlugin({
+      filename: './dist/css/styles.css'
+    }),
+    new PurifyCSSPlugin({
+      paths: glob.sync([
+        path.join(__dirname, 'dist/index.html'),
+        path.join(__dirname, 'src/js/*.js')
+      ])
+    })
+  ]
+}
